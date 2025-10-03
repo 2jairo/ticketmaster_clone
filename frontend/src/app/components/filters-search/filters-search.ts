64c1d6049741841edffc99326d6f79df02f21278
4jairo <jairotecles@gmail.com>
2025-10-03 14:25:53 +0200

    tmp

---
+++ frontend/src/app/components/filters-search/filters-search.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-filters-search',
+  imports: [],
+  templateUrl: './filters-search.html'
+})
+export class FiltersSearch {
+
+}
