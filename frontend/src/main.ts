1fc6405fc2046f38dda04806afa3d47868e95fbf
4jairo <jairotecles@gmail.com>
2025-10-12 10:25:26 +0200

    frontend: optimize picocss

--- frontend/src/main.ts
+++ frontend/src/main.ts
@@ -2,7 +2,6 @@ import { bootstrapApplication } from '@angular/platform-browser';
 import { appConfig } from './app/app.config';
 import { App } from './app/app';
 import { register } from 'swiper/element';
-import '@picocss/pico'
 
 register()
 
