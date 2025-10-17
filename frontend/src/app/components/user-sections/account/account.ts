93a5cc4eea539c7146608d1293738cd6a1cb8098
4jairo <jairotecles@gmail.com>
2025-10-17 13:12:01 +0200

    frontend: /user/account logout section

--- frontend/src/app/components/user-sections/account/account.ts
+++ frontend/src/app/components/user-sections/account/account.ts
@@ -1,6 +1,8 @@
-import { Component } from '@angular/core';
+import { Component, ElementRef, inject, Renderer2, ViewChild } from '@angular/core';
 import { AccountInfoForm } from "./account-info-form";
 import { PasswordsForm } from "./passwords-form";
+import { UserAuthService } from '../../../services/userAuth.service';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-account',
@@ -8,5 +10,39 @@ import { PasswordsForm } from "./passwords-form";
   templateUrl: './account.html'
 })
 export class Account {
+  private renderer2 = inject(Renderer2)
+  private userAuthService = inject(UserAuthService)
+  private router = inject(Router)
 
+  private removeDocumentClickListener!: () => void
+
+
+  @ViewChild('logout') logoutDialog!: ElementRef<HTMLDialogElement>
+  @ViewChild('logoutContent') logoutDialogContent!: ElementRef<HTMLElement>
+
+  closeLogoutDialog() {
+    if(this.removeDocumentClickListener) {
+      this.removeDocumentClickListener()
+    }
+    this.logoutDialog.nativeElement.close()
+  }
+
+  openLogoutDialog(e: PointerEvent) {
+    e.stopPropagation()
+
+    this.logoutDialog.nativeElement.showModal()
+
+    this.removeDocumentClickListener = this.renderer2.listen(document, 'click', (e) => {
+
+      if(!this.logoutDialogContent.nativeElement.contains(e.target)) {
+        this.closeLogoutDialog()
+      }
+    })
+  }
+
+  handleLogout() {
+    this.userAuthService.logout()
+    this.closeLogoutDialog()
+    this.router.navigate(['/'])
+  }
 }
